e7c1e2cd1e124ecdb9be140b48375afcb613378a
jianfei <wujianfei1991@gmail.com>
2016-01-05 12:31:53 +0800

    1123

--- template/音乐模板.js
+++ template/音乐模板.js
@@ -1,39 +1,39 @@
-//<div class="music music_ani"></div>
+<div class="music music_ani"></div>
 
 
 
-// .music_ani {
-//     -webkit-animation: music 2s linear infinite;
-//     animation: music 2s linear infinite;
-// }
+.music_ani {
+    -webkit-animation: music 2s linear infinite;
+    animation: music 2s linear infinite;
+}
 
-// .music {
-//     width: 43px;
-//     height: 42px;
-//     position: absolute;
-//     top: 62px;
-//     right: 26px;
-//     z-index: 999;
-//     background: url('http://i3.yongche.name/u/images/201510/4022501597.png?1445541000') no-repeat center center;
-// }
-// @-webkit-keyframes music {
-//   0% {
-//     -webkit-transform:rotate(0deg);
-//   }
+.music {
+    width: 43px;
+    height: 42px;
+    position: absolute;
+    top: 62px;
+    right: 26px;
+    z-index: 999;
+    background: url('http://i3.yongche.name/u/images/201510/4022501597.png?1445541000') no-repeat center center;
+}
+@-webkit-keyframes music {
+  0% {
+    -webkit-transform:rotate(0deg);
+  }
 
-//   100% {
-//     -webkit-transform:rotate(360deg);
-//   }
-// }
-// @keyframes music {
-//   0% {
-//     transform:rotate(0deg);
-//   }
+  100% {
+    -webkit-transform:rotate(360deg);
+  }
+}
+@keyframes music {
+  0% {
+    transform:rotate(0deg);
+  }
 
-//   100% {
-//     transform:rotate(360deg);
-//   }
-// }
+  100% {
+    transform:rotate(360deg);
+  }
+}
 
 
 
@@ -49,9 +49,6 @@
 		}
    });
 
-
-
-   
 
 var bg = document.createElement('audio');
 bg.src = '';
