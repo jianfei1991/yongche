5aa582086e475513bb1d514dd6c54ac98cefe08e
wujianfei1991 <wujianfei1991@gmail.com>
2016-06-17 14:29:23 +0800

    PC last

--- template/common.js
+++ template/common.js
@@ -59,9 +59,9 @@ function setCookie(name, value){
     var expires = (argc > 2) ? argv[2] : null; 
     if(expires!=null){ 
         var LargeExpDate = new Date (); 
-        LargeExpDate.setTime(LargeExpDate.getTime() + (expires*1000*3600*24));         
+        LargeExpDate.setTime(LargeExpDate.getTime() + (expires*1000*3600*24));
     } 
-    document.cookie = name + "=" + escape (value)+((expires == null) ? "" : ("; expires=" +LargeExpDate.toGMTString())); 
+    document.cookie = name + "=" + escape (value)+((expires == null) ? "" : ("; expires=" +LargeExpDate.toGMTString()));
 }
 
 
